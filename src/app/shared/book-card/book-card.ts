9559c59797615bcec7252a54ab8f155c3924b930
Iren <irennabil1.10.2006@gmail.com>
2026-09-08 21:47:45 +0300

    navbar style edit

--- src/app/shared/book-card/book-card.ts
+++ src/app/shared/book-card/book-card.ts
@@ -1,20 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 import { Book } from '../../core/models/book.model';
 
 @Component({
   selector: 'app-book-card',
   standalone: true,
+  imports: [CurrencyPipe],
   templateUrl: './book-card.html',
   styleUrl: './book-card.css'
 })
 export class BookCard {
-  
-
   @Input() book!: Book;
-
   @Input() badge?: string;
   @Input() badgeType: 'normal' | 'new' | 'sale' = 'normal';
-
   @Input() isWishlisted: boolean = false;
 
   @Output() addToCart = new EventEmitter<Book>();
--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
-export class Footer {
-
-}
+export class Footer {}
