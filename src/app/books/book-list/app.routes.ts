edebad187a723e1c45b38fa5f11090c108843221
Hana056 <hanamedhatk@gmail.com>
2026-09-10 11:20:02 +0300

    wishlist style changed

--- src/app/books/book-list/app.routes.ts
+++ src/app/books/book-list/app.routes.ts
@@ -1,18 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [{
-path:"browse-books",
- loadComponent : ()=>
-    import('./browse-books/browse-books').then(m=>m.browsebooks)
-},
-{
-path:'book-details/:id',
-loadComponent:()=>import('./book-details/book-details').then(m=>m.bookdetails)
-
-},
-{
-    path:'',
-    redirectTo:'browse-books',
-    pathMatch:'full'
-}
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/books',
+    pathMatch: 'full'
+  }
 ];
